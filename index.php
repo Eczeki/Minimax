57bdfa8f91062cae519388ff362a6ed8d450cd99
Eczeki <vaihtlumin@gmail.com>
2015-05-22 16:14:20 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+    <script src="/js/play.js"></script>
+<link rel="stylesheet" type="text/css" href="/style/landing.css">
+</head>
+    <body>
+        <div class = centerBox>
+            <div class = gameTable>
+                <div id = "leftTopTile">
+                    </div>
+                <div id = leftMidTile>
+                    </div>
+                <div id = leftBotTile>
+                    </div>
+                <div id = midBotTile>
+                    </div> 
+                <div id = rightBotTile>
+                    </div>
+                <div id = centerTile>
+                    </div>    
+                <div id = rightMidTile>
+                    </div>
+                <div id = rightTopTile>
+                    </div>
+                <div id = topMidTile>
+                    </div>    
+                </div>
+        </div>
+    </body>
+</html>
